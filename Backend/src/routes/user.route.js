06e64493b716040eb509e362e8cfb2b32ab674ab
SadiwalSachin <162051873+SadiwalSachin@users.noreply.github.com>
2024-06-23 12:51:09 +0530

    Update user.route.js

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -3,7 +3,7 @@ import { loginUser, registerUser } from "../controller/user.controller.js";
 
 const router = Router()
 
-router.route("signup").post(registerUser)
+router.route("/signup").post(registerUser)
 router.route("/login", ).post(loginUser)
 
 export default router
